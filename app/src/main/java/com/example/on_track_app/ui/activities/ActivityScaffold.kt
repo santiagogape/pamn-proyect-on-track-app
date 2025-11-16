8c631b669f5440bfca640bcb7ade69bf8d027e4b
santiagogape <santiago.galindo101@alu.ulpgc.es>
2025-11-16 19:56:56 +0000

    activity for Project

--- app/src/main/java/com/example/on_track_app/ui/activities/ActivityScaffold.kt
+++ app/src/main/java/com/example/on_track_app/ui/activities/ActivityScaffold.kt
@@ -1,6 +1,5 @@
 package com.example.on_track_app.ui.activities
 
-import android.app.Activity
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
